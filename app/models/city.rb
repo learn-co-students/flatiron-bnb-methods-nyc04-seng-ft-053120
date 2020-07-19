8cabf83bfb06f00cb10dfe1906b8a46b8978cb07
peawarrior <jacksonchen171@gmail.com>
2020-07-19 15:43:45 -0400

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,29 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  def city_openings(start_date, end_date)
+    self.listings.select do |listing|
+      listing.reserved_dates.include?(start_date.to_date) || listing.reserved_dates.include?(end_date.to_date) ? false : true
+    end
+  end
+
+  def total_reservations
+    self.listings.sum do |listing|
+      listing.total_reservations
+    end
+  end
+
+  def self.highest_ratio_res_to_listings
+    self.all.max_by do |city|
+      city.total_reservations/city.listings.count
+    end
+  end
+
+  def self.most_res
+    self.all.max_by do |city|
+      city.total_reservations
+    end
+  end
+
 end
 
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,59 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, :checkout, presence: true
+  validate :host_is_not_guest?
+  validate :available?
+  validate :valid_date_range?
+
+  def date_range
+    date = self.checkin
+    range = []
+    until range.include?(self.checkout) do
+      range << date
+      date += 1
+    end
+    range
+  end
+
+  def reserved_date?(first_date, second_date)
+    self.date_range.include?(first_date) || self.date_range.include?(second_date) ? true : false
+  end
+
+  def duration
+    checkout - checkin
+  end
+
+  def total_price
+    (self.listing.price * self.duration).to_i
+  end
+
+  private
+
+  def host_is_not_guest?
+    if self.listing.host == self.guest
+      errors.add(:guest, "can't be host")
+    end
+  end
+
+  def available?
+    if self.listing.reserved_dates.include?(checkin)
+      errors.add(:checkin, "has been reserved")
+    end
+    if self.listing.reserved_dates.include?(checkout)
+      errors.add(:checkout, "has been reserved")
+    end
+  end
+
+
+  def valid_date_range?
+    if !checkin
+      errors.add(:checkin, "is invalid")
+    elsif !checkout
+      errors.add(:checkout, "is invalid")
+    elsif checkout - checkin <= 0
+      errors.add(:checkin, "must be before checkout date")
+    end
+  end
+
 end
